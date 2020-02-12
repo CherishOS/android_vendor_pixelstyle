a104aa7efb4bba5fb8f16f33b5c1952c4d9073e1
Arseniy Graur <argraur@aosip.dev>
2020-02-12 11:24:05 +0000

    pixelstyle: Import Pixel Sounds
    Signed-off-by: Arseniy Graur <argraur@aosip.dev>
    Change-Id: I666d6abc8fe31a6921bd0f5b2f4e60b400587fd1

--- config.mk
+++ config.mk
@@ -112,3 +112,6 @@ PRODUCT_COPY_FILES += \
 # Include product overlays
 PRODUCT_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
+
+# Inherit from audio config
+$(call inherit-product, vendor/pixelstyle/config/audio.mk)
